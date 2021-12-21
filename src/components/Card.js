f9837eee767f74136b1739ecc820a58cea17d13f
facundo diaz <facundodiaz25@gmail.com>
2021-12-20 23:25:01 -0300

    first commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,16 +5,16 @@ import '../assets/css/styles.css'
 function Card({ item }) {
     const {title, description, image, price} = item;
     return (
-                    <div className="col-md-4 p-2">
-                        <div className="card cardd">
-                        <img src={image} className="card-img-top card-img" alt="card-img-top"/>
-                        <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text small text-card">{description}</p>
-                            <p className="card-text pt-5 "><small className="text-muted"> Price {price}</small></p>
-                        </div>
-                        </div>
-                     </div>
+        <div className="col-md-4 p-2">
+            <div className="card cardd">
+                <img src={image} className="card-img-top card-img" alt="card-img-top"/>
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text small text-card">{description}</p>
+                    <p className="card-text pt-5 "><small className="text-muted"> Price {price}</small></p>
+                </div>
+            </div>
+        </div>
        
     );
 }
